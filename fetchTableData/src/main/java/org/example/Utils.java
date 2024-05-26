e19f3377e44bae2ce523823161192f12251e07b7
Veronicagonsalves96 <veronicagonsalves96@gmail.com>
2024-05-26 14:21:07 +0530

    "UpdatedPropertiesFile"

---
+++ fetchTableData/src/main/java/org/example/Utils.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Utils {
+    public String reader(String key) throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("ConfigFile.properties");
+        try {
+
+            if (inputStream == null) {
+                throw new FileNotFoundException("Config file 'ConfigFile.properties' not found in the classpath");
+            }
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
+        Properties props=new Properties();
+        props.load(inputStream);
+
+        props.getProperty(key);
+        return key;
+    }
+}
